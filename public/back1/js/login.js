e13ed2a6805338775a6f11ffb554ead57bd03f6d
tsw529 <504134896@qq.com>
2019-02-22 20:21:18 +0800

    登录页面全部完成

--- public/back1/js/login.js
+++ public/back1/js/login.js
@@ -15,7 +15,6 @@ $(function () {
         fields: {
             // 用户名
             username: {
-                message: '用户名验证失败',
                 validators: {
                     //非空
                     notEmpty: {
@@ -26,11 +25,13 @@ $(function () {
                         min: 2,
                         max: 6,
                         message: '用户名长度为2-6位'
+                    },
+                    callback:{
+                        message:'用户名不存在'
                     }
                 }
             },
             password:{
-                message:'密码验证失败',
                 validators: {
                     //非空
                     notEmpty:{
@@ -41,6 +42,9 @@ $(function () {
                         min: 6,
                         max: 12,
                         message: '密码长度为6-12位',
+                    },
+                    callback:{
+                        message:'密码错误'
                     }
                 }
             }
@@ -65,10 +69,16 @@ $(function () {
          dataType:'json',
          success:function(info){
              if(info.error === 1000){
-                 alert('用户名不存在');
+          // 调用插件实例方法, 更新username字段状态成失败状态
+          // updateStatus( field, status, validator );
+          // 参数1: 需要更新的字段名称
+          // 参数2: 需要更新成的状态  VALID 成功  INVALID 失败
+          // 参数3: 配置校验规则, 将来会用配置的规则的 message 进行提示
+          $("#form").data('bootstrapValidator').updateStatus('username', 'INVALID', 'callback');
              }
              if(info.error === 1001){
-                 alert('密码错误') ;
+          $("#form").data('bootstrapValidator').updateStatus('password', 'INVALID', 'callback');
+                
              }
              if(info.success){
                  //登录成功
@@ -77,4 +87,13 @@ $(function () {
          }
      })
  })
+
+//  3.表单重置功能
+    //    reset 按钮, 本身就可以重置内容, 所以此时只需要重置状态即可
+    //    resetForm(false);  只重置状态
+    //    resetForm(true);   重置内容和状态
+    $('[type="reset"]').click(function(){
+        //重置状态
+        $("#form").data('bootstrapValidator').resetForm();
+    })
 }) 
